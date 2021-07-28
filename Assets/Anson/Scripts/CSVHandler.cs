b4e30d5449b3f5a2475aab873d13bdd290e774b4
Psycho_Nudalz <anson.aw0618@gmail.com>
2021-07-28 18:16:58 +0100

    Anson: move card data location

---
+++ Assets/Anson/Scripts/CSVHandler.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CSVHandler : MonoBehaviour
+{
+    string[] headerFormat = { "Card ID", "Card Details", "Ages Neede", "Status Needed", "Card Sprite Name", "Card Description",
+        "Heads Description","Heads Health","Heads Bux","Heads Mood", "Heads Required Status","Heads Sequence Cards Add","Heads Sequence Cards Remove", "Heads Status Add","Heads Status Remove",
+        "Tails Description","Tails Health","Tails Bux","Tails Mood", "Tails Required Status","Tails Sequence Cards Add","Tails Sequence Cards Remove", "Tails Status Add","Tails Status Remove",
+    };
+
+
+}
