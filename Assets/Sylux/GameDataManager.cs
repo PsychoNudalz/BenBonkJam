323cc1e52413c7a469a37dae7d308e231a41638f
Psycho_Nudalz <anson.aw0618@gmail.com>
2021-07-29 14:31:33 +0100

    Anson: fixed save

--- Assets/Sylux/GameDataManager.cs
+++ Assets/Sylux/GameDataManager.cs
@@ -12,18 +12,31 @@ public class GameDataManager : MonoBehaviour
 
         Achievements achievements = new Achievements();
         EndingsUnlocked endingsUnlocked = new EndingsUnlocked();
-       // Achievements.gamesPlayed = 0;
+        // Achievements.gamesPlayed = 0;
 
 
         string json = JsonUtility.ToJson(achievements);
         string json2 = JsonUtility.ToJson(endingsUnlocked);
-        Debug.Log(json);
-        Debug.Log(json2);
 
-        File.WriteAllText(Application.dataPath + "saveFile.json", json);
+        try
+        {
 
-      //  Achievements loadedAchievements = JsonUtility.FromJson<Achievements>(json);
-      //  EndingsUnlocked loadedEndingsUnlocked = JsonUtility.FromJson<EndingsUnlocked>(json);
+            File.WriteAllText(Application.persistentDataPath + "/Save/saveFile.json", json);
+            Debug.Log(Application.persistentDataPath);
+            Debug.Log(json);
+            Debug.Log(json2);
+        }
+        catch (DirectoryNotFoundException e)
+        {
+            Directory.CreateDirectory(Application.persistentDataPath + "/Save/");
+            File.WriteAllText(Application.persistentDataPath + "/Save/saveFile.json", json);
+
+            Debug.LogError("Save file error");
+            Debug.LogError(e.StackTrace);
+        }
+
+        //  Achievements loadedAchievements = JsonUtility.FromJson<Achievements>(json);
+        //  EndingsUnlocked loadedEndingsUnlocked = JsonUtility.FromJson<EndingsUnlocked>(json);
 
 
     }
@@ -37,6 +50,22 @@ public class GameDataManager : MonoBehaviour
         public int easterEggsFound;
         public int endingsFound;
         public int uniqueCardsDiscovered;
+
+        public Achievements(int gamesPlayed, int easterEggsFound, int endingsFound, int uniqueCardsDiscovered)
+        {
+            this.gamesPlayed = gamesPlayed;
+            this.easterEggsFound = easterEggsFound;
+            this.endingsFound = endingsFound;
+            this.uniqueCardsDiscovered = uniqueCardsDiscovered;
+        }
+
+        public Achievements()
+        {
+            this.gamesPlayed = 0;
+            this.easterEggsFound = 0;
+            this.endingsFound = 0;
+            this.uniqueCardsDiscovered = 0;
+        }
     }
 
     private class EndingsUnlocked
