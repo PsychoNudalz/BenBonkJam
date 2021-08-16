f06cddcb89e89481d9eb68ce0b6fcf82d935103e
Altair <altairrobinson@hotmail.com>
2021-08-16 23:10:05 +0100

    Sound Manager Changes

--- Assets/Sylux/Audio/SoundManager.cs
+++ Assets/Sylux/Audio/SoundManager.cs
@@ -2,31 +2,79 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using System;
+using System.IO;
 
 public class SoundManager : MonoBehaviour
 {
     public static SoundManager soundManager;
     public AudioMixer audioMixer;
-    public float sFXVolume;
-    public float musicVolume;
+    public float musicSliderValueFloat;
+    public float SFXsliderValueFloat;
 
-    void Start()
+    [SerializeField] SavedSettings savedSettings = new SavedSettings();
+    public SavedSettings SavedSettings1 { get => savedSettings; set => savedSettings = value; }
+
+    private void Awake()
     {
-        
+        ReadFromSave();
     }
 
-    void Update()
+    private void ReadFromSave()
     {
-        
+        string json = JsonUtility.ToJson(savedSettings);
+        try
+        {
+            Debug.Log("Loading sound settings");
+            json = File.ReadAllText(Application.persistentDataPath + "/Save/savedSettings.json");
+        }
+        catch (System.Exception)
+        {
+            Debug.Log("settingsSaved not found");
+            Directory.CreateDirectory(Application.persistentDataPath + "/Save/");
+            File.WriteAllText(Application.persistentDataPath + "/Save/savedSettings.json", json);
+
+          //  Debug.LogError("Save file error");
+            //   Debug.LogError(Exception.StackTrace);
+
+        }
+
+        savedSettings = JsonUtility.FromJson<SavedSettings>(json);
+
+
     }
 
-    public void SetMusicLevel(float sliderValue)
+    private void WriteToSave()
     {
-        audioMixer.SetFloat("Music", Mathf.Log10(sliderValue) * 20);
+        Debug.Log("Writing Save");
+        string json = JsonUtility.ToJson(savedSettings);
+
+        File.WriteAllText(Application.persistentDataPath + "/Save/savedSettings.json", json);
+        Debug.Log(Application.persistentDataPath);
+        Debug.Log(json);
+
+        Debug.Log("Saved settings.");
+
+
+        //  Achievements loadedAchievements = JsonUtility.FromJson<Achievements>(json);
+        //  EndingsUnlocked loadedEndingsUnlocked = JsonUtility.FromJson<EndingsUnlocked>(json);
     }
 
-    public void SetSFXLevel(float sliderValue)
+    public void SetMusicLevel(float musicSliderValue)
     {
-        audioMixer.SetFloat("SFX", Mathf.Log10(sliderValue) * 20);
+        audioMixer.SetFloat("Music", Mathf.Log10(musicSliderValue) * 20);
+        musicSliderValueFloat = musicSliderValue;
     }
+
+    public void SetSFXLevel(float SFXsliderValue)
+    {
+        audioMixer.SetFloat("SFX", Mathf.Log10(SFXsliderValue) * 20);
+        SFXsliderValueFloat = SFXsliderValue;
+    }
+}
+
+public class SavedSettings
+{
+    public float musicSliderValue;
+
 }
