95e9dea0dc4a095a85ad7540b0f1b2e823a05706
Daniel Newsom <danielgnewsom@gmail.com>
2021-08-20 17:05:07 +0100

    Danny - Add particle effects to status icon spawner

--- Assets/Danny/Scripts/StatusEffectIconSpawner.cs
+++ Assets/Danny/Scripts/StatusEffectIconSpawner.cs
@@ -6,6 +6,7 @@ public class StatusEffectIconSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject EffectIconGO;
     [SerializeField] private Transform endLocationObject;
+    [SerializeField] private ParticleSystem statusEffectParticles;
 
     private Vector3 endPosition;
 
@@ -15,11 +16,23 @@ public class StatusEffectIconSpawner : MonoBehaviour
     {
         endPosition = endLocationObject.position;
         FindObjectOfType<UIHandler>().UpdateStatusDisplay(FindObjectOfType<Player>());
+        if(statusEffectParticles == null)
+        {
+            statusEffectParticles = GameObject.Find("StatusParticleSystem").GetComponent<ParticleSystem>();
+        }
     }
 
     public void SpawnStatusEffectIcon(StatusEffect status)
     {
         GameObject icon = Instantiate(EffectIconGO, transform.position, Quaternion.identity, this.transform);
         icon.GetComponent<StatusEffectIcon>().SetIcon(status.icon);
+        statusEffectParticles.Play();
+    }
+
+    [ContextMenu("Spawn test icon")]
+    public void SpawnTestIcon()
+    {
+        StatusEffect testSE = FindObjectOfType<StatusEffectManager>().GetStatusEffect(StatusEnum.Addict);
+        SpawnStatusEffectIcon(testSE);
     }
 }
