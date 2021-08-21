c43b857cddbdd3c1b02e23205adb4f53cf6dd3d9
Psycho_Nudalz <anson.aw0618@gmail.com>
2021-08-21 12:34:27 +0100

    Anson: Remove duplicate zookeeper

--- Assets/Anson/Scripts/GameOver_StatusDisplay.cs
+++ Assets/Anson/Scripts/GameOver_StatusDisplay.cs
@@ -1,25 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameOver_StatusDisplay : MonoBehaviour
 {
     [SerializeField] GameOver_StatusStatus baseGO;
     [SerializeField] Transform scrollViewContent;
+    [SerializeField] List<StatusEffect> savedStatuses;
+    [SerializeField] float timeBetweenStatuses = 0.5f;
+    [SerializeField] Scrollbar verticalBar;
 
     public void Initialise(StatusEffect[] statusEffects)
     {
-        GameOver_StatusStatus newSS;
-        foreach (StatusEffect se in statusEffects)
-        {
-            newSS = Instantiate(baseGO.gameObject, scrollViewContent).GetComponent<GameOver_StatusStatus>();
-            newSS.Initialise(se);
-        }
-
-
-        RectTransform rt = scrollViewContent.GetComponent<RectTransform>();
-        rt.sizeDelta = new Vector2(rt.sizeDelta.x, baseGO.GetComponent<RectTransform>().rect.height * statusEffects.Length);
-        
-
+        savedStatuses.AddRange(statusEffects);
+        StartCoroutine(DisplayStatus());
     }
+
+
+
+
+    private GameOver_StatusStatus InitialiseStatusEffect(StatusEffect se)
+    {
+        GameOver_StatusStatus newSS = Instantiate(baseGO.gameObject, scrollViewContent).GetComponent<GameOver_StatusStatus>();
+        newSS.Initialise(se);
+        RectTransform rt = scrollViewContent.GetComponent<RectTransform>();
+        rt.sizeDelta = new Vector2(rt.sizeDelta.x, baseGO.GetComponent<RectTransform>().rect.height * scrollViewContent.childCount);
+        verticalBar.value = 1f;
+        return newSS;
+    }
+
+
+    IEnumerator DisplayStatus()
+    {
+        InitialiseStatusEffect(savedStatuses[0]).StartAnimation();
+        savedStatuses.RemoveAt(0);
+        yield return new WaitForSeconds(timeBetweenStatuses);
+        if (savedStatuses.Count > 0)
+        {
+            StartCoroutine(DisplayStatus());
+        }
+    }
+
 }
--- Assets/Anson/Scripts/GameOver_StatusStatus.cs
+++ Assets/Anson/Scripts/GameOver_StatusStatus.cs
@@ -9,13 +9,49 @@ public class GameOver_StatusStatus : MonoBehaviour
     [SerializeField] Image image;
     [SerializeField] TextMeshProUGUI statsText;
     [SerializeField] TextMeshProUGUI score;
+    [Header("Animation")]
+    [Range(0f, 1f)]
+    [SerializeField] float rangeValue;
+    [SerializeField] bool playAnimation;
+    [SerializeField] float animationTime = 1f;
 
 
-    
-    public void Initialise(StatusEffect se) 
+    public void Initialise(StatusEffect se)
     {
         image.sprite = se.icon;
         statsText.text = se.status.ToString();
         score.text = StatusToScore.GetScore(se.status).ToString();
     }
+
+    private void Update()
+    {
+        PlayAnimation();
+    }
+
+    private void PlayAnimation()
+    {
+        if (playAnimation)
+        {
+            image.color = ScaleColour(image.color);
+            statsText.color = ScaleColour(statsText.color);
+            score.color = ScaleColour(score.color);
+            rangeValue += animationTime * Time.deltaTime;
+            if (rangeValue >= 1f)
+            {
+                playAnimation = false;
+            }
+        }
+    }
+
+    Color ScaleColour(Color color)
+    {
+        return new Vector4(color.r, color.g, color.b, rangeValue);
+    }
+
+    public void StartAnimation()
+    {
+        playAnimation = true;
+    }
+
+
 }
